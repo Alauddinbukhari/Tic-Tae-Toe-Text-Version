5b851896d43af7e2d65607fb87c4e87514b48899
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2024-02-12 08:26:16 -0700

    :

--- main.py
+++ main.py
@@ -1,125 +1,99 @@
-from player import  Player
-from patterns_positions import patterns
-#import a dictionary which have speicific postions that can help us determine a winner
-def fetch_position(player):
-    positions_list=[]
-    for i, row in enumerate(game_array):
-        for j, element in enumerate(row):
-            if element == player.choice:
-                positions_list.append([i,j]) #Todo : check if it is returning a tuple or not?
-    layout()
-    posi_tuples = to_three_element(positions_list)
-    if posi_tuples :
-        return check_pattern(posi_tuples)
-    #if pattern found then  it is true else go for another player
+from player import Player
+from patterns_positions import winning_positions
+from itertools import combinations
 
 
-#issue is that i der can be many elements
-#after getting positions i must shuffle them in three different components so that i can use it with "if in "
+# Initialize game array
+game_array = [["", "", ""], ["", "", ""], ["", "", ""]]
 
-
-
-def check_pattern(tuples):
-    if len(tuples) != 3:
-        return None
-    for _ in tuples:
-        if tuples in  patterns:
-            return True
-
-
-# if pattern in tuple
-#return true
-
-
-
-def to_three_element(list_of_positions):
-
-
-    if len(list_of_positions)==3:
-        new_tuple=(element for element in list_of_positions)
-        #((1,2),(2,3),(4,5)) c
-    elif len(list_of_positions)>3:
-        #give me all combination
-
-    return None
-
-
-    #this algo should be able to shuffle if der are more then 3 elements and give all combinations
-
-
-#return a multidimentional tuple
-
-
-
+# Function to display the game layout
 def layout():
     for row in game_array:
         print(row)
 
+# Function to fetch positions of a player's choice
+def fetch_position(player):
+    positions_list = []
 
-def ask_position(player):
-    print(f"{player.name} your turn. ")
+    for i, row in enumerate(game_array):
+        for j, element in enumerate(row):
+            if element == player.choice:
+                positions_list.append((i, j))
+
     layout()
-    if "X" not in game_array and 0 not in game_array:
+    posi_tuples = to_three_element(positions_list)
 
-        print("given the above gamelayout, give me the positions in row and column,")
-        print("starting from one each.")
+    if posi_tuples:
+        return check_pattern(posi_tuples)
 
+# Function to check if a pattern exists in a tuple of positions
+def check_pattern(tuples):
+    if len(tuples) != 3:
+        return None
 
-#add a functionality that can check if a element is alreADY present at given positon
+    for tpl in tuples:
+        if tpl in patterns:
+            return True
 
-    row=int(input("Give me row number"))-1
-    column= int(input("Give me column number"))-1
-    game_array[row][column]=player.choice
-    return fetch_position(player)
-
-
-
-
-
-
-
-# def turn_list_to_diagram:
-#     pass
-
-
-
+# Function to convert a list of positions into three-element tuples
+def to_three_element(list_of_positions):
+    if len(list_of_positions) == 3:
+        return tuple(list_of_positions)
+    elif len(list_of_positions) > 3:
+        # return all combinations of three elements
+        return tuple(combinations(list_of_positions, 3))
+def combination():
 
 
 
-#Main
-game_array=[["","",""],
-            ["","",""],
-            ["","",""]]
 
-player_1=Player("player 1")
-player_2=Player("player 2")
+# Function to take player input for position
+def ask_position(player):
+    if all(cell != "" for row in game_array for cell in row):
+        print("It's a draw!")
+        return None
+
+    print(f"{player.name}, it's your turn.")
+    layout()
 
 
-player_1.choice=input("Player 1,choose between 0 or X\n")
-if player_1.choice =="X":
-    player_2.choice=0
 
-else:
+    row = int(input("Enter row number: ")) - 1
+    column = int(input("Enter column number: ")) - 1
 
-    player_2.choice="X"
-    print(f"Player 2 your weapon is {player_2.choice}" )
-
-gameover=False
-player2_result=""
-while not gameover:
-    if "" not in game_array:
-        gameover=True
-        print("Draw")
-    player1_result=ask_position(player_1)
-
-    if player1_result:
-        gameover=True
-        print("Winner is ")
+    if game_array[row][column] == "":
+        game_array[row][column] = player.choice
+        return fetch_position(player)
     else:
-        player2_result=ask_position(player_2)
-        if player2_result:
-            gameover=True
-            print("Winner is player 2")
+        print("Cell already taken. Try again.")
+        return ask_position(player)
 
 
 
+
+# Main game loop
+player_1 = Player("Player 1")
+player_2 = Player("Player 2")
+
+player_1.choice = input("Player 1, choose between 0 or X: ")
+
+if player_1.choice == "X":
+    player_2.choice = "0"
+else:
+    player_2.choice = "X"
+    print(f"Player 2, your weapon is {player_2.choice}")
+
+game_over = False
+
+while not game_over:
+    player_1_result = ask_position(player_1)
+
+    if player_1_result:
+        game_over = True
+        print(f"{player_1.name} is the winner!")
+    else:
+        player_2_result = ask_position(player_2)
+
+        if player_2_result:
+            game_over = True
+            print(f"{player_2.name} is the winner!")
